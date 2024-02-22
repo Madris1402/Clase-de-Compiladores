2dbcd05d2788a51aaff8fbfb4f3c0c0af1c44278
Madris1402 <ricmad142@gmail.com>
2024-02-21 21:43:39 -0600

    Lector de cadenas de texto arreglado

--- Programas/src/fes/aragon/profe/cadenasTexto/Inicio.java
+++ Programas/src/fes/aragon/profe/cadenasTexto/Inicio.java
@@ -8,12 +8,11 @@ public class Inicio {
         try {
             int verifica = app.inicio();
             if (verifica == 1) {
-                System.out.println("Cadena valida");
+                System.out.print("Cadena valida");
             }else {
-                System.out.println("Cadena invalida");
+                System.out.print("Cadena invalida");
             }
         } catch (Exception e) {
-            // TODO Auto-generated catch block
             System.out.println(e);
         }
     }
