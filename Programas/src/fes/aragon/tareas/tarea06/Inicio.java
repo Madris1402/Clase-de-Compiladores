0b4a3ad5ba3edee1078f9ab68c4dac76b320e347
Madris1402 <ricmad142@gmail.com>
2024-02-26 21:02:59 -0600

    Tarea 06 Lector de Matrices v1

---
+++ Programas/src/fes/aragon/tareas/tarea06/Inicio.java
@@ -0,0 +1,17 @@
+package fes.aragon.tareas.tarea06;
+
+import fes.aragon.tareas.tarea06.herramientas.Archivos;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+
+		Archivos archivo = new Archivos();
+		archivo.analizarPalabras();
+		
+
+		
+		
+	}
+
+}
