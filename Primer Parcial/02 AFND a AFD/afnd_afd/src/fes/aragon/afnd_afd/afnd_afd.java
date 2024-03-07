6a437a010f764cbe97b000eee60d0107d3032190
Madris1402 <ricmad142@gmail.com>
2024-03-07 00:14:31 -0600

    Programa 02 Automata No Determinista a Automata Determinista (IF)

---
+++ Primer Parcial/02 AFND a AFD/afnd_afd/src/fes/aragon/afnd_afd/afnd_afd.java
@@ -0,0 +1,72 @@
+package fes.aragon.afnd_afd;
+
+import fes.aragon.herramientas.Gestor;
+public class afnd_afd {
+
+    public static void main(String[] args) {
+
+        for (String palabra : Gestor.leer("fuente.txt")) {
+            int indice = 0;
+            int estado = 0;
+            String mensajeError = "";
+            int columna = 1;
+
+            while (indice <= palabra.length() - 1) {
+                char c = palabra.charAt(indice);
+
+                if (c != '0' && c != '1') {
+                    mensajeError = "contiene el Caracter no válido: " + c + " en la columna " + columna;
+                    break;
+                }
+
+                switch (estado) {
+                    case 0:
+                        if (c == '0') {
+                            estado = 0;
+                        } else if (c == '1') {
+                            estado = 1;
+                        }
+                        break;
+
+                    case 1:
+                        if (c == '0') {
+                            estado = 1;
+                        } else if (c == '1') {
+                            estado = 2;
+                        }
+                        break;
+
+                    case 2:
+                        if (c == '0') {
+                            estado = 3;
+                        } else if (c == '1') {
+                            estado = 2;
+                        }
+                        break;
+
+                    case 3:
+                        if (c == '0') {
+                            estado = 3;
+                        } else if (c == '1') {
+                            estado = 2;
+                        }
+                        break;
+                }
+                indice++;
+                columna++;
+            }
+
+            if (mensajeError.isEmpty()) {
+                if (estado == 2) {
+                    System.out.println(palabra + ":" + " Cadena Valida");
+                } else if (estado == 3){
+                    System.out.println(palabra + ":" + " Cadena Valida");
+                } else{
+                    System.out.println(palabra + ":" + " Cadena Invalida");
+                }
+            } else {
+                System.out.println("La Palabra \"" + palabra + "\": " + mensajeError);
+            }
+        }
+    }
+}
