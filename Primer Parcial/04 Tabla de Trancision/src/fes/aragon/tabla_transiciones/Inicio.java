04c39911dda5a28529bf53753558977ae9855293
Madris1402 <ricmad142@gmail.com>
2024-03-11 00:12:53 -0600

    Programa 04 Tabla de Trancisiones (Palabras con Matrices)

---
+++ Primer Parcial/04 Tabla de Trancision/src/fes/aragon/tabla_transiciones/Inicio.java
@@ -0,0 +1,17 @@
+package fes.aragon.tabla_transiciones;
+
+import fes.aragon.herramientas.Archivos;
+
+public class Inicio {
+
+	public static void main(String[] args) {
+
+		Archivos archivo = new Archivos();
+		archivo.analizarPalabras();
+		
+
+		
+		
+	}
+
+}
