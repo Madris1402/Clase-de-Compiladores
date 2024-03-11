f942a43dd50f84c9331e30bf6412e62014dabd96
Madris1402 <ricmad142@gmail.com>
2024-03-10 19:37:19 -0600

    Programa 02 Automata Finito No Determinista a Automata Finito Determinista (if statements)

--- Primer Parcial/02 AFND a AFD/src/fes/aragon/afnd_afd/afnd_afd.java
+++ Primer Parcial/02 AFND a AFD/src/fes/aragon/afnd_afd/afnd_afd.java
@@ -1,11 +1,15 @@
 package fes.aragon.afnd_afd;
 
 import fes.aragon.herramientas.Gestor;
+
 public class afnd_afd {
 
-    public static void main(String[] args) {
+    private static String archivo = "fuente.txt";
 
-        for (String palabra : Gestor.leer("fuente.txt")) {
+    public static void main (String[]args){
+
+
+        for (String palabra : Gestor.leer(archivo)) {
             int indice = 0;
             int estado = 0;
             String mensajeError = "";
@@ -15,16 +19,16 @@ public class afnd_afd {
                 char c = palabra.charAt(indice);
 
                 if (c != '0' && c != '1') {
-                    mensajeError = "contiene el Caracter no válido: " + c + " en la columna " + columna;
+                    mensajeError = "contiene el Caracter no valido: " + c + " en la columna " + columna;
                     break;
                 }
 
                 switch (estado) {
                     case 0:
                         if (c == '0') {
-                            estado = 0;
-                        } else if (c == '1') {
                             estado = 1;
+                        } else if (c == '1') {
+                            estado = 2;
                         }
                         break;
 
@@ -38,15 +42,22 @@ public class afnd_afd {
 
                     case 2:
                         if (c == '0') {
-                            estado = 3;
+                            estado = 1;
                         } else if (c == '1') {
-                            estado = 2;
+                            estado = 3;
                         }
                         break;
 
                     case 3:
                         if (c == '0') {
+                            estado = 4;
+                        } else if (c == '1') {
                             estado = 3;
+                        }
+                        break;
+                    case 4:
+                        if (c == '0') {
+                            estado = 4;
                         } else if (c == '1') {
                             estado = 2;
                         }
@@ -57,11 +68,9 @@ public class afnd_afd {
             }
 
             if (mensajeError.isEmpty()) {
-                if (estado == 2) {
+                if (estado == 3 || estado == 4) {
                     System.out.println(palabra + ":" + " Cadena Valida");
-                } else if (estado == 3){
-                    System.out.println(palabra + ":" + " Cadena Valida");
-                } else{
+                } else {
                     System.out.println(palabra + ":" + " Cadena Invalida");
                 }
             } else {
