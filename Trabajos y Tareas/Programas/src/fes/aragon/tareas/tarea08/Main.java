7157faa73c2b20132cd249c2c4dc3186cb758612
Madris1402 <ricmad142@gmail.com>
2024-03-13 22:47:58 -0600

    Correcciones hechas en la clase de 12 de marzo

--- Trabajos y Tareas/Programas/src/fes/aragon/tareas/tarea08/Main.java
+++ Trabajos y Tareas/Programas/src/fes/aragon/tareas/tarea08/Main.java
@@ -3,19 +3,73 @@ import fes.aragon.tareas.tarea08.Lexico;
 
 import java.io.*;
 public class Main {
+    private Lexico lexico;
+    private Tokens token;
     public static void main(String[] args){
+        Main app = new Main();
+
         try {
             Reader rd = new BufferedReader(new FileReader(System.getProperty("user.dir") + File.separator + "files" + File.separator + "tarea08.fes"));
-            Lexico lexico = new Lexico(rd);
-            Tokens resultado;
-            do{
-                resultado = lexico.yylex();
-                System.out.println(resultado);
-            }while (resultado != null);
+                app.lexico = new Lexico(rd);
+                app.token = app.lexico.yylex();
+                app.S();
         } catch (FileNotFoundException e){
             e.printStackTrace();
         } catch (IOException e){
             e.printStackTrace();
         }
     }
+
+    private void S() throws IOException{
+        System.out.println("S");
+        E();
+        token = lexico.yylex();
+        if (token != Tokens.PUNTOYCOMA){
+            System.out.println("Error, se esperaba ;");
+        }
+        return;
+    }
+    private void E() throws IOException{
+        System.out.println("E");
+        T();
+        Ep();
+        return;
+    }
+
+    private void Ep() throws IOException {
+        if (token == Tokens.OR){
+            token = lexico.yylex();
+            T();
+            Ep();
+        }else{
+            System.out.println("Error, se espera or");
+        }
+    }
+
+    private void T() throws IOException{
+        System.out.println("T");
+        F();
+        Tp();
+    }
+
+    private void Tp() throws IOException {
+        if(token == Tokens.AND){
+            F();
+            Tp();
+        } else {
+            System.out.println("Error, se espera and");
+        }
+    }
+
+    private void F() throws IOException{
+        System.out.println("F");
+
+        if (token == Tokens.NOT){
+            token = lexico.yylex();
+            F();
+        } else{
+            return;
+        }
+    }
+
 }
